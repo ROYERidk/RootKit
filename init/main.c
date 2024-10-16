c788d2ff8c39b1d330ad4f3735c85559e58855fb
ROYERidk <royeretudes@gmail.com>
2024-10-16 13:13:37 +0200

    adding base main init C prog

---
+++ init/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#define MY_SYSCALL_NUMBER 548 // depend of your syscall
+
+int main()
+{
+    printf("ECOLE 2600 MY INIT\n");
+    syscall(MY_SYSCALL_NUMBER);
+    while (2600)
+    {
+        pid_t pid = fork();
+        int status = 0;
+        if (pid)
+        {
+            waitpid(pid, &status, 0);
+            printf("Respawn\n");
+            pid = 0;
+        }
+        else
+        {
+            char *tab[] = {"/usr/bin/setsid", "cttyhack", "sh", NULL};
+            execv("/usr/bin/setsid", tab);
+        }
+    }
+}
