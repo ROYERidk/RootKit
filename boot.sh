f5164f623250b92aa310072b72c1eed16750fb42
ROYERidk <royeretudes@gmail.com>
2024-10-16 13:18:21 +0200

    script working well, Linux booting

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo qemu-system-x86_64 -kernel ./linux-6.10.10/arch/x86/boot/bzImage -initrd ./initramfs.cpio.gz -append "console=ttyS0" -nographic
+sudo qemu-system-x86_64 -kernel ./linux/arch/x86/boot/bzImage -initrd ./initramfs.cpio.gz -append "console=ttyS0" -nographic
